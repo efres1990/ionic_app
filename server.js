f009da54641aab838a9fc8452bbdc02cc9fea477
efres1990 <e.freires@alumnos.urjc.es>
2017-11-26 19:55:39 +0100

    subieno

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var app = express();
 app.use(express.static(path.resolve(__dirname, "www")));
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
- console.log("listening to Port", app.get("port"));
+ console.log("listening to Port", app.get("port" ));
 });
