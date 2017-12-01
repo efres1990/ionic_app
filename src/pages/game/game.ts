a6790e7c2eb58a6c4937b46da789ae7c85b8bb75
efres1990 <e.freires@alumnos.urjc.es>
2017-12-01 01:39:24 +0100

    change splash margin, new function to game chest

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
-import {ChestDataProvider} from '../../providers/chest-data/chest-data';
+import { ChestDataProvider } from '../../providers/chest-data/chest-data';
 /**
  * Generated class for the GamePage page.
  *
@@ -16,26 +16,42 @@ import {ChestDataProvider} from '../../providers/chest-data/chest-data';
 })
 export class GamePage {
   public img: any;
-  chests: Array<Object> = [];  
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams,public chestData:ChestDataProvider) {
-    this.img='super-magical-chest-close.png';
-    this.chestData.getChest().then(arenaList => {
-      this.chests = arenaList;
-      console.log("Chest " + this.chests);
+  chests: Array<Object> = [];
+  chestsFirst: Array<Object> = [];
+  public premio: any;
 
-    })
+  constructor(public navCtrl: NavController, public navParams: NavParams, public chestData: ChestDataProvider) {
+    this.img = 'super-magical-chest-close.png';
+    this.chests = [];
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GamePage');
   }
   ionViewDidLeave() {
-    this.img='super-magical-chest-close.png';
-    
-     }
+    this.img = 'super-magical-chest-close.png';
+    this.chests = [];
+    this.chestsFirst = [];
+  }
   public openChest() {
     this.img = 'super-magical-chest-open.png';
-   
+    this.chestData.getChest().then(chestList => {
+      console.log(chestList);
+      this.chests = chestList.slice(4, 8);
+      this.chestsFirst = chestList.slice(0, 4);
+      this.premio="!!PREMIOOOO¡¡";
+      //this.getSecondArray();
+      console.log(chestList + " First " + this.chestsFirst + "Segundo " + this.chests);
+
+    })
   }
+  /*public getSecondArray() {
+    for (let i = 0; i < this.chests.length/2; i++) {
+      this.chests.shift();
+      this.chestsFirst.pop();
+      
+    }
+  }*/
+
 }
